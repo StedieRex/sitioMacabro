3fd0c92646f2dc47bc6819fe0c5d9d2682789f02
Luis Alberto Pacheco Espinoza <luis.pachecoe@alumno.buap.mx>
2024-07-19 15:25:40 -0600

    transicion al estilo galeria

---
+++ cosos.js
@@ -0,0 +1,16 @@
+// script.js
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.gallery img');
+const totalImages = images.length;
+
+document.getElementById('nextBtn').addEventListener('click', () => {
+    images[currentImageIndex].classList.remove('active');
+    currentImageIndex = (currentImageIndex + 1) % totalImages;
+    images[currentImageIndex].classList.add('active');
+});
+
+document.getElementById('prevBtn').addEventListener('click', () => {
+    images[currentImageIndex].classList.remove('active');
+    currentImageIndex = (currentImageIndex - 1 + totalImages) % totalImages;
+    images[currentImageIndex].classList.add('active');
+});
